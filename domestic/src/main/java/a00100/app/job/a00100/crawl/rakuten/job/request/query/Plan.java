22daf4586d88490ed6a91d89f908bff56e1e1d3f
hidek <hidek@NT001>
2021-07-08 06:43:42 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/job/request/query/Plan.java
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/job/request/query/Plan.java
@@ -1,8 +1,18 @@
 package a00100.app.job.a00100.crawl.rakuten.job.request.query;
 
-import a00100.app.job.a00100.crawl.rakuten.job.webBrowser.WebClient;
-import lombok.experimental.Accessors;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import a00100.app.job.a00100.crawl.rakuten.job.webBrowser.Attributes;
+import a00100.app.job.a00100.crawl.rakuten.job.webBrowser.WebClient;
+import lombok.val;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Accessors(prefix = "m_", chain = true)
 class Plan extends WebClient {
 	static final ThreadLocal<Plan> m_instances = new ThreadLocal<Plan>() {
@@ -26,8 +36,10 @@ class Plan extends WebClient {
 
 	public WebClient execute() throws Exception {
 		try {
-			for (m_current = new _00000(); m_current != null;) {
-				m_current = (_Current) m_current.execute();
+			for (val r : query()) {
+				for (m_current = r; m_current != null;) {
+					m_current = (_Current) m_current.execute();
+				}
 			}
 
 			return null;
@@ -36,9 +48,44 @@ class Plan extends WebClient {
 		}
 	}
 
+	Collection<_00000> query() throws Exception {
+		return new ArrayList<_00000>() {
+			{
+				val driver = getWebDriver();
+				val by = By.xpath("//ul[contains(@class,'htlPlnRmTypLst')]/li");
+
+				for (val element : driver.findElements(by)) {
+					add(new _00000() {
+						{
+							m_element = element;
+						}
+					});
+				}
+			}
+		};
+	}
+
 	static class _Current extends WebClient {
 	}
 
 	static class _00000 extends _Current {
+		String m_hotelNo;
+		WebElement m_element;
+
+		String getHotelNo() throws Exception {
+			if (m_hotelNo == null) {
+				val by = By.name("f_no");
+				val element = m_element.findElement(by);
+				m_hotelNo = element.getAttribute(Attributes.VALUE);
+			}
+
+			return m_hotelNo;
+		}
+
+		@Override
+		public WebClient execute() throws Exception {
+			System.out.println(getHotelNo());
+			return super.execute();
+		}
 	}
 }
---
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/job/webBrowser/Attributes.java
@@ -0,0 +1,5 @@
+package a00100.app.job.a00100.crawl.rakuten.job.webBrowser;
+
+public class Attributes {
+	public static final String VALUE = "value";
+}
