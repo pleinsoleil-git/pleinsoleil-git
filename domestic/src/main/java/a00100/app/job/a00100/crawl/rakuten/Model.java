852277ce27860cad7bec8e3552d0dfb959e2a9ad
hidek <hidek@NT001>
2021-07-07 16:01:22 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/Model.java
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/Model.java
@@ -2,24 +2,20 @@ package a00100.app.job.a00100.crawl.rakuten;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Model extends a00100.app.job.Model {
-	static AtomicBoolean m_executing = new AtomicBoolean();
+	static AtomicBoolean m_running = new AtomicBoolean();
 
 	public void execute() throws Exception {
 		if (isExecutable() == true) {
 			try {
-				log.info("@@@@@@@@@@@@@");
 			} finally {
-				m_executing.set(false);
+				m_running.set(false);
 			}
 		}
 	}
 
 	boolean isExecutable() {
-		return m_executing.compareAndSet(false, true);
+		return m_running.compareAndSet(false, true);
 	}
 
 	void job() throws Exception {
