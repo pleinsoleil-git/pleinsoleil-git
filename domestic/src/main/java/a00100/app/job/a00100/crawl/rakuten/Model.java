422e318895ac1911d1e5e2db4240a75e8864e3fc
hidek <hidek@NT001>
2021-07-07 15:53:38 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/Model.java
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/Model.java
@@ -1,6 +1,27 @@
 package a00100.app.job.a00100.crawl.rakuten;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Model extends a00100.app.job.Model {
+	static AtomicBoolean m_executing = new AtomicBoolean();
+
 	public void execute() throws Exception {
+		if (isExecutable() == true) {
+			try {
+				log.info("@@@@@@@@@@@@@");
+			} finally {
+				m_executing.set(false);
+			}
+		}
+	}
+
+	boolean isExecutable() {
+		return m_executing.compareAndSet(false, true);
+	}
+
+	void job() throws Exception {
 	}
 }
---
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/job/request/Request.java
@@ -0,0 +1,40 @@
+package a00100.app.job.a00100.crawl.rakuten.job.request;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import lombok.val;
+import lombok.experimental.Accessors;
+
+@Accessors(prefix = "m_", chain = true)
+public class Request {
+	static Request m_instance;
+	_Current m_current;
+
+	public static Request getInstance() {
+		return (m_instance == null ? m_instance = new Request() : m_instance);
+	}
+
+	public void execute() throws Exception {
+		try {
+			for (val r : query()) {
+				(m_current = r).execute();
+			}
+		} finally {
+			m_instance = null;
+		}
+	}
+
+	Collection<_Current> query() throws Exception {
+		return new ArrayList<_Current>() {
+			{
+				add(new _Current());
+			}
+		};
+	}
+
+	public static class _Current {
+		void execute() throws Exception {
+		}
+	}
+}
