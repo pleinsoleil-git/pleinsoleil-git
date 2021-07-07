86332c87e810a0b3874468d5265dcf58cb814b92
hidek <hidek@NT001>
2021-07-07 18:21:51 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/job/webBrowser/WebBrowser.java
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/job/webBrowser/WebBrowser.java
@@ -1,12 +1,22 @@
 package a00100.app.job.a00100.crawl.rakuten.job.webBrowser;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import a00100.app.job.a00100.crawl.rakuten.job.Job;
 import lombok.val;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class WebBrowser implements AutoCloseable {
 	static WebBrowser m_instance;
+	ConcurrentHashMap<Long, _Current> m_currents;
 
 	static {
 		val job = Job.getCurrent();
@@ -15,13 +25,71 @@ public class WebBrowser implements AutoCloseable {
 	}
 
 	WebBrowser() {
+		m_currents = new ConcurrentHashMap<Long, _Current>();
 	}
 
 	public static WebBrowser getInstance() {
 		return (m_instance == null ? m_instance = new WebBrowser() : m_instance);
 	}
 
+	public static _Current getCurrent() {
+		val k = Thread.currentThread().getId();
+		_Current v = m_instance.m_currents.get(k);
+
+		if (v == null) {
+			m_instance.m_currents.put(k, v = new _Current());
+		}
+
+		return v;
+	}
+
 	@Override
 	public void close() throws Exception {
+		try {
+			for (val v : m_currents.values()) {
+				v.close();
+			}
+		} finally {
+			m_currents.clear();
+			m_instance = null;
+		}
+	}
+
+	public static class _Current implements AutoCloseable {
+		File m_directory;
+		WebDriver m_webDriver;
+
+		public WebDriver getWebDriver() throws Exception {
+			if (m_webDriver == null) {
+				m_webDriver = new ChromeDriver(new ChromeOptions() {
+					{
+						addArguments("--incognito"); // シークレットモード
+						setExperimentalOption("prefs", new HashMap<String, Object>() {
+							{
+								// ==================================================
+								// ダウンロード先を設定
+								// ==================================================
+								put("download.default_directory", getDownloadDirectory().getPath());
+							}
+						});
+					}
+				});
+			}
+
+			return m_webDriver;
+		}
+
+		public File getDownloadDirectory() throws Exception {
+			if (m_directory == null) {
+				m_directory = Files.createTempDirectory(null).toFile();
+				log.debug(String.format("download.default_directory[%s]", m_directory.getPath()));
+			}
+
+			return m_directory;
+		}
+
+		@Override
+		public void close() throws Exception {
+		}
 	}
 }
---
+++ domestic/src/main/java/common/io/TempDirectory.java
@@ -0,0 +1,47 @@
+package common.io;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.apache.commons.io.FileUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TempDirectory implements AutoCloseable {
+	File m_dir;
+
+	public TempDirectory(final File dir) {
+		m_dir = dir;
+	}
+
+	public static TempDirectory create() throws Exception {
+		return create(null);
+	}
+
+	public static TempDirectory create(final String prefix) throws Exception {
+		return new TempDirectory(Files.createTempDirectory(prefix).toFile());
+	}
+
+	File attach(final File dir) {
+		try {
+			return m_dir;
+		} finally {
+			m_dir = dir;
+		}
+	}
+
+	File detach() {
+		return attach(null);
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (m_dir != null) {
+			if (m_dir.exists() == true) {
+				log.debug(String.format("Delete derectory[%s]", m_dir.getPath()));
+				FileUtils.forceDelete(m_dir);
+			}
+		}
+	}
+}
