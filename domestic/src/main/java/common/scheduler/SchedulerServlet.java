5fd94953365a1ab9acd1ac8570e966d9ee8d8bb7
hidek <hidek@NT001>
2021-07-07 09:27:32 +0900

    a

---
+++ domestic/src/main/java/common/scheduler/SchedulerServlet.java
@@ -0,0 +1,14 @@
+package common.scheduler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SchedulerServlet extends HttpServlet {
+	@Override
+	public void init() throws ServletException {
+System.out.println("@@@@@@@@@@@@@@@@@@@");
+	}
+}
