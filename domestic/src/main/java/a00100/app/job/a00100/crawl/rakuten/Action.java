9b6799e5473c2ba81d26f79625a633aae6984930
hidek <hidek@NT001>
2021-07-08 17:01:10 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/Action.java
+++
@@ -1,7 +0,0 @@
-package a00100.app.job.a00100.crawl.rakuten;
-
-public class Action extends a00100.app.job.Action {
-	public Action() {
-		super(new App());
-	}
-}
--- domestic/src/main/java/a00100/app/job/a00100/crawl/rakuten/App.java
+++
@@ -1,7 +0,0 @@
-package a00100.app.job.a00100.crawl.rakuten;
-
-public class App extends a00100.app.job.App {
-	public App() {
-		super(new Model());
-	}
-}
