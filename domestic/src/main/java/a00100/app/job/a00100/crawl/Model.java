1a0fcba90e5d6274e4db72d31cc6c65debcaf47b
hidek <hidek@NT001>
2021-07-08 13:51:19 +0900

    a

---
+++ domestic/src/main/java/a00100/app/job/a00100/crawl/Model.java
@@ -0,0 +1,28 @@
+package a00100.app.job.a00100.crawl;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import a00100.app.job.a00100.crawl.rakuten.job.Job;
+import lombok.val;
+
+public class Model extends a00100.app.job.Model {
+	static AtomicBoolean m_running = new AtomicBoolean();
+
+	public void execute() throws Exception {
+		if (isExecutable() == true) {
+			try (val conn = Connection.getInstance()) {
+				job();
+			} finally {
+				m_running.set(false);
+			}
+		}
+	}
+
+	boolean isExecutable() {
+		return m_running.compareAndSet(false, true);
+	}
+
+	void job() throws Exception {
+		Job.getInstance().execute();
+	}
+}
