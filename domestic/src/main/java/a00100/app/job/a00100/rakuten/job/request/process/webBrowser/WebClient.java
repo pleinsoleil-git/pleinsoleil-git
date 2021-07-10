ab60b811a037c26ea33c06837974b1de447bc2ef
hidek <hidek@NT001>
2021-07-10 17:01:10 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/rakuten/job/request/process/webBrowser/WebClient.java
+++ domestic/src/main/java/a00100/app/job/a00100/rakuten/job/request/process/webBrowser/WebClient.java
@@ -2,9 +2,16 @@ package a00100.app.job.a00100.rakuten.job.request.process.webBrowser;
 
 import org.openqa.selenium.WebDriver;
 
+import a00100.app.job.a00100.rakuten.job.request.process.connection.Connection;
+import common.jdbc.JDBCConnection;
+
 public class WebClient {
 	public WebDriver getWebDriver() throws Exception {
 		return WebBrowser.getCurrent().getWebDriver();
+	}
+
+	public JDBCConnection getConnection() {
+		return Connection.app();
 	}
 
 	public WebClient execute() throws Exception {
