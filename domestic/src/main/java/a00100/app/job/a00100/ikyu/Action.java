31700d255e4cd72ece825293e65bef2f8fae7a73
hidek <hidek@NT001>
2021-07-11 08:42:56 +0900

    a

---
+++ domestic/src/main/java/a00100/app/job/a00100/ikyu/Action.java
@@ -0,0 +1,7 @@
+package a00100.app.job.a00100.ikyu;
+
+public class Action extends a00100.app.job.Action {
+	public Action() {
+		super(new App());
+	}
+}
---
+++ domestic/src/main/java/a00100/app/job/a00100/ikyu/App.java
@@ -0,0 +1,7 @@
+package a00100.app.job.a00100.ikyu;
+
+public class App extends a00100.app.job.App {
+	public App() {
+		super(new Model());
+	}
+}
