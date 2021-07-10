470aae06c49df7ed40a31c8dc33b0fe40cfdffd1
hidek <hidek@NT001>
2021-07-10 17:22:14 +0900

    a

---
+++ domestic/src/main/java/a00100/app/job/a00100/rakuten/job/request/process/crawl/Crawl.java
@@ -0,0 +1,30 @@
+package a00100.app.job.a00100.rakuten.job.request.process.crawl;
+
+import a00100.app.job.a00100.rakuten.job.request.process.crawl.login.Login;
+import a00100.app.job.a00100.rakuten.job.request.process.webBrowser.WebClient;
+
+public class Crawl {
+	static final ThreadLocal<Crawl> m_instances = new ThreadLocal<Crawl>() {
+		@Override
+		protected Crawl initialValue() {
+			return new Crawl();
+		}
+	};
+
+	Crawl() {
+	}
+
+	public static Crawl getInstance() {
+		return m_instances.get();
+	}
+
+	public void execute() throws Exception {
+		try {
+			for (WebClient client = Login.getInstance(); client != null;) {
+				client = client.execute();
+			}
+		} finally {
+			m_instances.remove();
+		}
+	}
+}
