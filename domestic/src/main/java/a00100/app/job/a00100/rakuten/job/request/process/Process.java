795c76f71b9c8c7431c1780096b171a91320a5c0
hidek <hidek@NT001>
2021-07-10 16:06:41 +0900

    a

--- domestic/src/main/java/a00100/app/job/a00100/rakuten/job/request/process/Process.java
+++ domestic/src/main/java/a00100/app/job/a00100/rakuten/job/request/process/Process.java
@@ -1,4 +1,4 @@
-package a00100.app.job.a00100.rakuten.job.request;
+package a00100.app.job.a00100.rakuten.job.request.process;
 
 import java.util.Collection;
 
@@ -6,8 +6,7 @@ import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.apache.commons.lang3.BooleanUtils;
 
-import a00100.app.job.a00100.rakuten.job.Job;
-import a00100.app.job.a00100.rakuten.job.request.process.Process;
+import a00100.app.job.a00100.rakuten.job.request.Request;
 import common.app.job.JobStatus;
 import common.jdbc.JDBCParameterList;
 import common.jdbc.JDBCUtils;
@@ -18,15 +17,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Accessors(prefix = "m_", chain = false)
-public class Request {
-	static Request m_instance;
+public class Process {
+	static Process m_instance;
 	_Current m_current;
 
-	Request() {
+	Process() {
 	}
 
-	public static Request getInstance() {
-		return (m_instance == null ? m_instance = new Request() : m_instance);
+	public static Process getInstance() {
+		return (m_instance == null ? m_instance = new Process() : m_instance);
 	}
 
 	public static _Current getCurrent() {
@@ -47,34 +46,43 @@ public class Request {
 		String sql;
 		sql = "WITH s_params AS\n"
 			+ "(\n"
-				+ "SELECT ?::BIGINT AS job_id,\n"
-					+ "?::VARCHAR AS request_type\n"
+				+ "SELECT ?::BIGINT AS request_id\n"
 			+ ")\n"
-			+ "SELECT j20.id,\n"
-				+ "j20.request_type AS requestType,\n"
-				+ "j20.request_name AS requestName\n"
+			+ "SELECT j30.id,\n"
+				+ "j20.job_type AS jobType,\n"
+				+ "j10.request_type AS requestType,\n"
+				+ "j10.user_id AS userId,\n"
+				+ "j10.password,\n"
+				+ "j20.check_in_date AS checkInDate,\n"
+				+ "j20.check_out_date AS checkOutDate,\n"
+				+ "j20.room_nums AS roomNums,\n"
+				+ "j20.adult_nums AS adultNums,\n"
+				+ "j20.upper_grade_nums AS upperGradeNums,\n"
+				+ "j20.lower_grade_nums AS lowerGradeNums,\n"
+				+ "j30.hotel_code AS hotelCode\n"
 			+ "FROM s_params AS t10\n"
-			+ "INNER JOIN j_crawl_job AS j10\n"
-				+ "ON j10.id = t10.job_id\n"
+			+ "INNER JOIN j_crawl_request AS j10\n"
+				+ "ON j10.id = t10.request_id\n"
 				+ "AND j10.aborted = FALSE\n"
-			+ "INNER JOIN j_crawl_request AS j20\n"
-				+ "ON j20.foreign_id = j10.id\n"
-				+ "AND j20.request_type = t10.request_type\n"
+			+ "INNER JOIN j_crawl_job AS j20\n"
+				+ "ON j20.id = j10.foreign_id\n"
 				+ "AND j20.aborted = FALSE\n"
-				+ "AND j20.deleted = FALSE\n"
+			+ "INNER JOIN j_crawl_process AS j30\n"
+				+ "ON j30.foreign_id = j10.id\n"
+				+ "AND j30.aborted = FALSE\n"
+				+ "AND j30.deleted = FALSE\n"
 			+ "WHERE NOT EXISTS\n"
 			+ "(\n"
 				+ "SELECT NULL\n"
-				+ "FROM j_crawl_request_status AS j900\n"
-				+ "WHERE j900.foreign_id = j20.id\n"
+				+ "FROM j_crawl_process_status AS j900\n"
+				+ "WHERE j900.foreign_id = j30.id\n"
 			+ ")\n";
 
 		val rs = new BeanListHandler<_Current>(_Current.class);
 		return JDBCUtils.query(sql, rs, new JDBCParameterList() {
 			{
-				val job = Job.getCurrent();
-				add(job.getId());
-				add(job.getRequestType());
+				val request = Request.getCurrent();
+				add(request.getId());
 			}
 		});
 	}
@@ -82,8 +90,6 @@ public class Request {
 	@Data
 	public static class _Current {
 		Long m_id;
-		String m_requestType;
-		String m_requestName;
 		Status m_status;
 
 		Status getStatus() {
@@ -91,14 +97,11 @@ public class Request {
 		}
 
 		void execute() throws Exception {
-			log.info(String.format("Request[id=%d type=%s name=%s]", getId(), getRequestType(), getRequestName()));
-
 			try (val status = getStatus()) {
 				try {
 					if (aborted() == true) {
 						status.setStatus(JobStatus.ABORT);
 					} else {
-						process();
 						status.setStatus(JobStatus.SUCCESS);
 					}
 				} catch (Exception e) {
@@ -109,23 +112,22 @@ public class Request {
 			}
 		}
 
-		void process() throws Exception {
-			Process.getInstance().execute();
-		}
-
 		boolean aborted() throws Exception {
 			String sql;
 			sql = "WITH s_params AS\n"
 				+ "(\n"
-					+ "SELECT ?::BIGINT AS request_id\n"
+					+ "SELECT ?::BIGINT AS process_id\n"
 				+ ")\n"
 				+ "SELECT j10.aborted\n"
 				+ "FROM s_params AS t10\n"
-				+ "INNER JOIN j_crawl_request AS j10\n"
-					+ "ON j10.id = t10.request_id\n"
-				+ "INNER JOIN j_crawl_job AS j20\n"
+				+ "INNER JOIN j_crawl_process AS j10\n"
+					+ "ON j10.id = t10.process_id\n"
+				+ "INNER JOIN j_crawl_request AS j20\n"
 					+ "ON j20.id = j10.foreign_id\n"
-					+ "AND j20.aborted = FALSE\n";
+					+ "AND j20.aborted = FALSE\n"
+				+ "INNER JOIN j_crawl_job AS j30\n"
+					+ "ON j30.id = j20.foreign_id\n"
+					+ "AND j30.aborted = FALSE\n";
 
 			val rs = new ScalarHandler<Boolean>();
 			return BooleanUtils.isTrue(JDBCUtils.query(sql, rs, new JDBCParameterList() {
